d61e4e5b56b9ccd90f82302188aa4b4abf2749c5
fatih.akbayrak <fatih.akbayrak@pirireis.com.tr>
2022-05-18 20:43:53 +0300

    refactor: add TextInputProps to text input component

--- apps/translator-app/src/app/components/Input/TextInput.tsx
+++ apps/translator-app/src/app/components/Input/TextInput.tsx
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
 import { TextInput as Input, View, TextInputProps } from 'react-native';
 
-interface Props extends TextInputProps {
-  placeholder: string;
-  onChangeInput: (text: string) => void;
-}
+interface Props extends TextInputProps {}
 
 export const TextInput = (props: Props) => {
-  const { placeholder, onChangeInput } = props;
-
   return (
     <View style={{ padding: 10 }}>
       <Input
-        onChangeText={onChangeInput}
+        {...props}
         multiline={true}
         placeholderTextColor="red"
-        placeholder={placeholder}
         style={{
           height: 200,
           padding: 20,
           paddingTop: 20,
           fontSize: 18,
-          backgroundColor: 'skyblue',
+          backgroundColor: 'papayawhip',
           borderRadius: 10,
         }}
       />
