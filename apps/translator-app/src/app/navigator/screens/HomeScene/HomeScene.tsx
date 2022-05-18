fbb1014cc338afc6b5950f971c520b9583c0b677
fatih.akbayrak <fatih.akbayrak@pirireis.com.tr>
2022-05-18 12:47:24 +0300

    feat: add useMutation to homescene

--- apps/translator-app/src/app/navigator/screens/HomeScene/HomeScene.tsx
+++ apps/translator-app/src/app/navigator/screens/HomeScene/HomeScene.tsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
-import { View, Text, Button } from 'react-native';
+import { View, ScrollView, Button } from 'react-native';
 import { TextInput } from '../../../components/Input/TextInput';
 import { ResultView } from './ResultView/ResultView';
 import { Header } from './Header/Header';
+import { useMutation, QueryClient } from 'react-query';
+import axiosInstance from '../../../utils/axios';
+import { useDispatch } from 'react-redux';
+import { useAppSlice } from '../../../slice/translates';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { NavigatorParamList } from '../../AppNavigator';
+const queryClient = new QueryClient();
 
 interface TranslationMode {
   mode: string;
@@ -12,32 +19,93 @@ interface TranslationMode {
 export const HomeScene = (props: TranslationMode) => {
   const { mode } = props;
 
-  const [text, setText] = useState('');
+  const inputRef = useRef();
+  //
+  const [inputText, setInputText] = useState('');
+  const [resultText, setResultText] = useState();
+  //
   const [changeMode, setChangeMode] = useState(mode);
+
+  const dispatch = useDispatch();
+  const navigation = useNavigation<NavigationProp<NavigatorParamList>>();
+
+  const {
+    actions: { saveTranslations },
+  } = useAppSlice();
+
+  const mutation = useMutation(
+    () => {
+      return fetch('https://libretranslate.de/translate', {
+        method: 'POST',
+        body: JSON.stringify({
+          q: inputText,
+          source: 'tr',
+          target: 'en',
+          format: 'text',
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status}`);
+        }
+        return res.json();
+      });
+    },
+    {
+      onMutate: (text) => {
+        console.log('Mutation started ', text);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+      onSettled: () => {},
+      onSuccess: (text) => {
+        console.log('result from the other side', text);
+        setResultText(text.translatedText);
+        dispatch(saveTranslations(text.translatedText));
+      },
+    }
+  );
 
   // would be tr-eng eng-tr vice versa
   const handleChangeTranslationMode = () => {};
 
-  console.log('sex', text);
+  const onSubmitText = () => {
+    mutation.mutate();
+    mutation.reset();
+  };
+
+  console.log('Input Text', inputText);
 
   return (
-    <View
-      style={{
-        flex: 1,
+    <ScrollView>
+      <View
+        style={{
+          flex: 1,
+          marginTop: 50,
+        }}
+      >
+        <Header
+          onChange={() => {
+            navigation.navigate('historyScene');
+          }}
+          title="Translator App"
+        />
 
-        marginTop: 50,
-      }}
-    >
-      <Header
-        onChange={() => handleChangeTranslationMode()}
-        title="Translator Application"
-      />
+        <TextInput onChangeInput={setInputText} placeholder="Enter text..." />
 
-      <TextInput onChangeInput={setText} placeholder="Enter text..." />
+        <View>
+          <Button
+            color={'red'}
+            onPress={() => onSubmitText()}
+            title="translate "
+          ></Button>
+        </View>
 
-      <View style={{ padding: 10, top: 10 }}>
-        <ResultView result="Translation..." />
+        <View style={{ padding: 10 }}>
+          <ResultView result={resultText} />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
